5e5ea0d8d1d062645a1e67c5a477340c87e24d99
renzhamin <renzhamin@gmail.com>
2023-05-28 15:19:22 +0600

    sign in with POST /api/auth/login and show relevant error messages

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,17 @@
-import { Button, Card, Input, Typography } from "@material-tailwind/react"
+import {
+    Alert,
+    Button,
+    Card,
+    Input,
+    Typography,
+} from "@material-tailwind/react"
 import React, { useState } from "react"
+import api from "../utils/api"
 
 const SignIn: React.FC = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
 
     const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUsername(e.target.value)
@@ -15,63 +23,86 @@ const SignIn: React.FC = () => {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        // Perform login logic here
+        const loginData = {
+            username,
+            password,
+        }
+        api.post("/auth/login", loginData)
+            .then(() => {})
+            .catch((e) => {
+                setError(e.response.data.error)
+                setTimeout(() => {
+                    setError("")
+                }, 2500)
+            })
     }
 
     return (
-        <Card
-            color="transparent"
-            shadow={false}
-            className="flex items-center justify-center h-screen"
-        >
-            <div className="w-96">
-                <Typography variant="h4" color="blue-gray" className="mb-6">
-                    Sign In
-                </Typography>
-                <div className="bg-white rounded-lg p-8">
-                    <form onSubmit={handleSubmit} className="space-y-6">
-                        <div>
-                            <Input
-                                id="username"
-                                size="lg"
-                                label="Email or Username"
-                                className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                                value={username}
-                                onChange={handleUsernameChange}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <Input
-                                type="password"
-                                id="password"
-                                size="lg"
-                                label="Password"
-                                className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                                value={password}
-                                onChange={handlePasswordChange}
-                                required
-                            />
-                        </div>
-                        <Button type="submit" className="mt-6" fullWidth>
-                            Sign In
-                        </Button>
-                        <Typography
-                            color="gray"
-                            className="mt-4 text-center font-normal"
-                        >
-                            Don't have an account?{" "}
-                            <a
-                                href="/signup"
-                                className="font-medium text-blue-500 transition-colors hover:text-blue-700"
+        <div>
+            <Card
+                color="transparent"
+                shadow={false}
+                className="flex items-center justify-center h-screen"
+            >
+                <div className="w-96">
+                    <Typography variant="h4" color="blue-gray" className="mb-6">
+                        Sign In
+                    </Typography>
+                    <div className="bg-white rounded-lg p-8">
+                        <form onSubmit={handleSubmit} className="space-y-6">
+                            <div>
+                                <Input
+                                    id="username"
+                                    size="lg"
+                                    label="Email or Username"
+                                    className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                                    value={username}
+                                    onChange={handleUsernameChange}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <Input
+                                    type="password"
+                                    id="password"
+                                    size="lg"
+                                    label="Password"
+                                    className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                    required
+                                />
+                            </div>
+                            <Button type="submit" className="mt-6" fullWidth>
+                                Sign In
+                            </Button>
+                            <Typography
+                                color="gray"
+                                className="mt-4 text-center font-normal"
                             >
-                                Sign Up
-                            </a>
-                        </Typography>
-                    </form>
+                                Don't have an account?{" "}
+                                <a
+                                    href="/signup"
+                                    className="font-medium text-blue-500 transition-colors hover:text-blue-700"
+                                >
+                                    Sign Up
+                                </a>
+                            </Typography>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </Card>
+            </Card>
+            {error && (
+                <div>
+                    <Alert
+                        color="red"
+                        className="absolute bottom-3 right-3 w-fit"
+                    >
+                        {error}
+                    </Alert>
+                </div>
+            )}
+        </div>
     )
 }
 
