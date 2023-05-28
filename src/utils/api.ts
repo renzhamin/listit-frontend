3d59e8e01d02176b7a3d31eaeaf888e396465e3a
renzhamin <renzhamin@gmail.com>
2023-05-28 15:27:47 +0600

    incldue Credentials in axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const api = axios.create({
     baseURL: "http://localhost:5000/api", // Replace with your actual base URL
+    withCredentials: true,
 })
 
 export default api
