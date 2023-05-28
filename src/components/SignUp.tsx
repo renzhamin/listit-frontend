1035e3e2d22d787d094c7260acc49583679518d3
renzhamin <renzhamin@gmail.com>
2023-05-28 13:45:37 +0600

    add retype password field

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import {
     Card,
     Input,
@@ -7,6 +8,34 @@ import {
 } from "@material-tailwind/react"
 
 export default function SignUp() {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [retypePassword, setRetypePassword] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+
+    const handleSignUp = (e) => {
+        e.preventDefault()
+
+        // Check if the passwords match
+        if (password !== retypePassword) {
+            setPasswordError("Passwords don't match")
+            return
+        }
+
+        // Perform the registration logic
+        const registrationData = {
+            username,
+            email,
+            password,
+        }
+
+        // Send a POST request to /api/auth/register with registrationData
+        // You can use libraries like Axios or fetch to make the API call
+
+        console.log("Registering:", registrationData)
+    }
+
     return (
         <Card color="transparent" shadow={false}>
             <Typography variant="h4" color="blue-gray">
@@ -15,31 +44,45 @@ export default function SignUp() {
             <Typography color="gray" className="mt-1 font-normal">
                 Enter your details to register.
             </Typography>
-            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+            <form
+                className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+                onSubmit={handleSignUp}
+            >
                 <div className="mb-4 flex flex-col gap-6">
-                    <Input size="lg" label="Name" />
-                    <Input size="lg" label="Email" />
-                    <Input type="password" size="lg" label="Password" />
+                    <Input
+                        size="lg"
+                        label="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Input
+                        size="lg"
+                        label="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        type="password"
+                        size="lg"
+                        label="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Input
+                        type="password"
+                        size="lg"
+                        label="Retype Password"
+                        value={retypePassword}
+                        onChange={(e) => setRetypePassword(e.target.value)}
+                        error={passwordError}
+                    />
                 </div>
-                <Checkbox
-                    label={
-                        <Typography
-                            variant="small"
-                            color="gray"
-                            className="flex items-center font-normal"
-                        >
-                            I agree the
-                            <a
-                                href="#"
-                                className="font-medium transition-colors hover:text-blue-500"
-                            >
-                                &nbsp;Terms and Conditions
-                            </a>
-                        </Typography>
-                    }
-                    containerProps={{ className: "-ml-2.5" }}
-                />
-                <Button className="mt-6" fullWidth>
+                {passwordError && (
+                    <Typography color="red" className="mb-4">
+                        {passwordError}
+                    </Typography>
+                )}
+                <Button className="mt-6" fullWidth type="submit">
                     Register
                 </Button>
                 <Typography
