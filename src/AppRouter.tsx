3dcafdf1dc15a41a938cd0bb367b27e67a1e52c9
renzhamin <renzhamin@gmail.com>
2023-05-28 13:36:28 +0600

    add signup

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./components/Home"
+import SignUp from "./components/SignUp"
 import About from "./components/About"
 import NotFound from "./components/NotFound"
 
@@ -8,7 +8,7 @@ const AppRouter: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<SignUp />} />
                 <Route path="/about" element={<About />} />
                 <Route element={<NotFound />} />
             </Routes>
