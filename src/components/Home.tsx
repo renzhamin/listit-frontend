acdc7dce6c89304714ba2dd9b980ae6be9132e65
Mohtasim-Hossain <86872837+Mohtasim-Hossain@users.noreply.github.com>
2023-05-31 10:41:37 +0600

    dialog box added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
+import {useState} from "react";
 import {
     List,
     ListItem,
@@ -9,15 +10,42 @@ import {
     CardHeader,
     Typography,
     Button,
+    Dialog,
+    DialogHeader,
+    DialogBody,
+    DialogFooter,
+    Input,
 } from "@material-tailwind/react"
 import { lists } from "../dummydata"
 
 const Home: React.FC = () => {
     const navigate = useNavigate()
+    const [isCreatingList, setIsCreatingList] = useState(false);
+    const [listTitle, setListTitle] = useState("");
 
-    const handleCreateList = () => {
-        navigate("/create-list")
-    }
+  const openCreateListDialog = () => {
+    setIsCreatingList(true);
+  };
+
+  const closeCreateListDialog = () => {
+    setIsCreatingList(false);
+    setListTitle("");
+  };
+
+  const handleListTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setListTitle(event.target.value);
+  };
+
+  const handleCreateList = () => {
+    console.log("List Title:", listTitle);
+    // Reset the form fields
+    setListTitle("");
+    navigate("/list");
+  };
+
+//     const handleCreateList = () => {
+//         navigate("/create-list")
+//     }
 
     return (
         <div>
@@ -45,13 +73,38 @@ const Home: React.FC = () => {
                     <Button
                         color="blue"
                         size="sm"
-                        onClick={handleCreateList}
+                        onClick={openCreateListDialog}
                         className="mt-2"
                     >
                         New List
                     </Button>
                 </List>
             </Card>
+            <Dialog size="sm" open={isCreatingList} handler={closeCreateListDialog}>
+        <DialogHeader>
+          Create New List
+        </DialogHeader>
+        <DialogBody>
+          <Input
+             type="text"
+             placeholder="Enter list title"
+             value={listTitle}
+             onChange={handleListTitleChange}
+          />
+        </DialogBody>
+        <DialogFooter className="flex w-max gap-4">
+           <Button
+             color="green"
+             onClick={handleCreateList}
+             disabled={!listTitle}
+           >
+             Create
+           </Button>
+           <Button color="gray" onClick={closeCreateListDialog}>
+             Cancel
+          </Button>
+        </DialogFooter>
+      </Dialog>
         </div>
     )
 }
