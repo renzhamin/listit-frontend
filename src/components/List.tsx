940743573d0ab62dcebcee98d270ca37abc4ace4
Mohtasim-Hossain <86872837+Mohtasim-Hossain@users.noreply.github.com>
2023-06-03 04:21:28 +0600

    edit and dialog added

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import {
     Accordion,
     Typography,
@@ -11,9 +11,11 @@ import {
     DialogBody,
     DialogHeader,
     DialogFooter,
+    Textarea,
+    Input,
 } from "@material-tailwind/react"
-import { lists } from "../dummydata"
-import { useParams } from "react-router-dom"
+import { lists } from "../dummydata";
+import { useParams } from "react-router-dom";
 
 function Icon({ id, open }) {
     return (
@@ -27,80 +29,166 @@ function Icon({ id, open }) {
             stroke="currentColor"
             strokeWidth={2}
         >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19 9l-7 7-7-7"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
         </svg>
-    )
+    );
 }
 
 const ShowList: React.FC = () => {
-    const [open, setOpen] = useState(-1)
-    const { id } = useParams()
+    const [open, setOpen] = useState(-1);
+    const { id } = useParams();
     const [deleteItemIndex, setDeleteItemIndex] = useState(false);
-    const handleOpen = (value: number) => {
-        setOpen(open === value ? 0 : value)
-    }
+    const [editItemIndex, setEditItemIndex] = useState(-1);
+    const [editedTitle, setEditedTitle] = useState("");
+    const [editedMainTitle, setEditedMainTitle] = useState("");
+    const [editedContent, setEditedContent] = useState("");
+    const [editingTitle, setEditingTitle] = useState(false);
+    const [addingItem, setAddingItem] = useState(false);
+    const [newItemTitle, setNewItemTitle] = useState("");
+    const [newItemContent, setNewItemContent] = useState("");
 
-    const list = lists.find((l) => l.id === id)
+    const handleOpen = (value: number) => {
+        setOpen(open === value ? 0 : value);
+    };
+
+    const list = lists.find((l) => l.id === id);
 
     if (!list) {
-        return <div>List not found</div>
+        return <div>List not found</div>;
     }
 
     const handleEdit = (itemIndex: number) => {
-        console.log(`Edit item ${itemIndex} of list ${list.id}`)
-    }
+        const item = list.content[itemIndex];
+        setEditItemIndex(itemIndex);
+        setEditedTitle(item.title);
+        setEditedContent(item.content);
+    };
+
+    const handleSaveEdit = () => {
+        // Perform save edit logic here
+        console.log(`Save edited item ${editItemIndex} of list ${list.id}`);
+        setEditItemIndex(-1);
+        setEditedTitle("");
+        setEditedContent("");
+    };
+
+    const handleCancelEdit = () => {
+        setEditItemIndex(-1);
+        setEditedTitle("");
+        setEditedContent("");
+    };
+
+
+    const handleEditTitle = () => {
+        // console.log(`Edit title of list ${list.id}`);
+        setEditingTitle(true);
+    setEditedMainTitle(list.title);
+    };
+
+    const handleSaveTitle = () => {
+        // Perform save title logic here
+        console.log(`Save edited title of list ${list.id}`);
+        setEditingTitle(false);
+        setEditedTitle("");
+      };
+
+
+      const handleCancelTitle = () => {
+        setEditingTitle(false);
+        setEditedTitle("");
+      };
+
 
     const openDeleteItemDialog = () => {
         setOpen(-1);
         setDeleteItemIndex(true);
-         
-      };
-    
-      const closeDeleteItemDialog = () => {
+    };
+
+    const closeDeleteItemDialog = () => {
         setOpen(-1);
-        setDeleteItemIndex(false); 
-      };
+        setDeleteItemIndex(false);
+    };
 
-    // const handleEdit = (itemIndex: number) => {
-    //     const updatedContent = prompt("Enter the updated content") || "";
-    //     const updatedList = { ...list };
-    //     updatedList.content[itemIndex].content = updatedContent;
-    //     setLists((prevLists) => {
-    //       const updatedLists = [...prevLists];
-    //       const listIndex = updatedLists.findIndex((l) => l.id === id);
-    //       updatedLists[listIndex] = updatedList;
-    //       return updatedLists;
-    //     });
-    //   };
-
-      const handleDelete = (itemIndex: number) => {
+    const handleDelete = (itemIndex: number) => {
         setDeleteItemIndex(itemIndex);
+    };
+
+    const confirmDelete = () => {
+        // Perform deletion logic here
+        setDeleteItemIndex(false);
+    };
+
+    const handleAddItems = () => {
+        setAddingItem(true);
       };
     
-      const confirmDelete = () => {
-        setLists((prevLists) => {
-          const updatedLists = [...prevLists];
-          const list = updatedLists.find((l) => l.id === id);
-          if (list) {
-            const updatedContent = [...list.content];
-            updatedContent.splice(deleteItemIndex, 1);
-            list.content = updatedContent;
-          }
-          return updatedLists;
-        });
-        setDeleteItemIndex(-1);
+      const handleSaveNewItem = () => {
+        // Perform save new item logic here
+        console.log(`Save new item for list ${list.id}`);
+        setNewItemTitle("");
+        setNewItemContent("");
+      };
+    
+      const handleCancelNewItem = () => {
+        setAddingItem(false);
+        setNewItemTitle("");
+        setNewItemContent("");
       };
 
     return (
         <div>
             <Card className="p-4 w-72 sm:w-[75vw] md:w-[65vw]">
-                <Typography variant="h3" className="mb-8">
-                    {list.title}
-                </Typography>
+               <Typography variant="h3" className="mb-8">
+          
+          {/* {editListIndex === } */}
+          {editingTitle ? (
+            <div>
+            <Input
+              variant="static"
+              label="Edit Title"
+              value={editedMainTitle}
+              onChange={(e) => setEditedMainTitle(e.target.value)}
+              style={{
+                width: "100%",
+                fontSize: "18px",
+              }}
+            />
+            
+                 <Button
+                    ripple={true}
+                    color="green"
+                    onClick={handleSaveTitle}
+                    size="sm"
+                    className="float-right"
+                      >
+                         Save
+                 </Button>
+                 <Button
+                    ripple={true}
+                    color="red"
+                    onClick={handleCancelTitle}
+                    size="sm"
+                    className="float-right mr-2"
+                      >
+                         Cancel
+                 </Button>
+            </div>
+            
+          ) : (
+            <>
+              {list.title}
+              <Button
+                ripple={true}
+                color="blue-gray"
+                onClick={handleEditTitle}
+                size="sm"
+                className="float-right"
+              >
+                Edit title
+              </Button>
+            </>
+          )}
+        </Typography>
                 {list.content.map((item, ind) => (
                     <div key={ind}>
                         <Accordion
@@ -111,67 +199,147 @@ const ShowList: React.FC = () => {
                             <AccordionHeader
                                 onClick={() => handleOpen(ind + 1)}
                             >
-                               <IconButton
+                                <IconButton
                                     color="red"
                                     onClick={openDeleteItemDialog}
                                     size="sm"
                                     ripple="dark"
-                                   >
+                                    style={{ minWidth: "30px" }}
+                                >
                                     <svg
-                                        xmlns="http://www.w3.org/2000/svg" 
+                                        xmlns="http://www.w3.org/2000/svg"
                                         width="16"
-                                        height="16" 
-                                        fill="currentColor" 
-                                        class="bi bi-trash" 
-                                        viewBox="0 0 16 16"> 
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                        height="16"
+                                        fill="currentColor"
+                                        class="bi bi-trash"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                                        />
                                     </svg>
                                 </IconButton>
-                                <Typography variant="h5">
-                                    {item.title}
+                                <Typography
+                                    variant="h5"
+                                    style={{
+                                        width: "100%",
+                                        margin: "0px 20px",
+                                    }}
+                                >
+                                    {editItemIndex === ind ? (
+                                        <Input
+                                            variant="static"
+                                            label="Edit Title"
+                                            value={editedTitle}
+                                            onChange={(e) =>
+                                                setEditedTitle(e.target.value)
+                                            }
+                                            style={{
+                                                width: "100%",
+                                                fontSize: "18px",
+                                            }}
+                                        />
+                                    ) : (
+                                        item.title
+                                    )}
                                 </Typography>
                             </AccordionHeader>
                             {item.content && (
-                                <AccordionBody>
-                                    <Button
-                                        ripple={true}
-                                        color="blue-gray"
-                                        onClick={() => handleEdit(ind)}
-                                        size="sm"
-                                        className="float-right"
-                                    >
-                                        Edit
-                                    </Button>
-                                    <Typography className="font-medium text-left pl-8">
-                                        {item.content}
-                                    </Typography>
+                                <AccordionBody
+                                    style={{
+                                        width: "100%",
+                                        padding: "20px 20px",
+                                    }}
+                                >
+                                    {editItemIndex === ind ? (
+                                        <Textarea
+                                            variant="static"
+                                            rows={8}
+                                            label="Edit Content"
+                                            value={editedContent}
+                                            onChange={(e) =>
+                                                setEditedContent(e.target.value)
+                                            }
+                                            resize
+                                            style={{height: "50px"}}
+                                        />
+                                    ) : (
+                                        <Typography className="font-medium text-left pl-8">
+                                            {item.content}
+                                        </Typography>
+                                    )}
+                                    {editItemIndex === ind ? (
+                                        <div>
+                                            <Button
+                                                ripple={true}
+                                                color="green"
+                                                onClick={handleSaveEdit}
+                                                size="sm"
+                                                className="float-right"
+                                            >
+                                                Save
+                                            </Button>
+                                            <Button
+                                                ripple={true}
+                                                color="red"
+                                                onClick={handleCancelEdit}
+                                                size="sm"
+                                                className="float-right mr-2"
+                                            >
+                                                Cancel
+                                            </Button>
+                                        </div>
+                                    ) : (
+                                        <Button
+                                            ripple={true}
+                                            color="blue-gray"
+                                            onClick={() => handleEdit(ind)}
+                                            size="sm"
+                                            className="float-right"
+                                        >
+                                            Edit
+                                        </Button>
+                                    )}
                                 </AccordionBody>
                             )}
                         </Accordion>
                     </div>
                 ))}
             </Card>
-            <Dialog size="sm" /*active={deleteItemIndex !== -1}*/ open={deleteItemIndex} handler={closeDeleteItemDialog}>
-        <DialogHeader>Delete Item</DialogHeader>
-        <DialogBody>
-          Are you sure you want to delete this item?
-        </DialogBody>
-        <DialogFooter>
-          <Button
-            color="red"
-            onClick={confirmDelete}
-            ripple="light"
-            className="mr-2"
-          >
-            Delete
-          </Button>
-          <Button color="gray" onClick={closeDeleteItemDialog} ripple="dark">
-            Cancel
-          </Button>
-        </DialogFooter>
-      </Dialog>
+            <Dialog
+                size="sm"
+                open={deleteItemIndex}
+                onClose={closeDeleteItemDialog}
+                style={{
+                    minWidth: "230px",
+                }}
+            >
+                <DialogHeader>Delete Item</DialogHeader>
+                <DialogBody>
+                    Are you sure you want to delete this item?
+                </DialogBody>
+                <DialogFooter>
+                    <Button
+                        color="red"
+                        onClick={confirmDelete}
+                        ripple="light"
+                        className="mr-2"
+                    >
+                        Delete
+                    </Button>
+                    <Button
+                        color="gray"
+                        onClick={closeDeleteItemDialog}
+                        ripple="dark"
+                    >
+                        Cancel
+                    </Button>
+                </DialogFooter>
+            </Dialog>
         </div>
     )
-}
+};
 
-export default ShowList
+export default ShowList;
